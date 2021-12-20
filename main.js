be67d15660b511c2ce8b2ffac3383330f6b75130
Davi-Coelho <davi.cmbarreto@gmail.com>
2021-12-19 23:06:20 -0400

    feature: electronJS adicionado e pre-configurado

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron')
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    try {
+        require('electron-reloader')(module, {
+            debug: true,
+            watchRenderer: true
+        });
+    } catch (_) { console.log('Error'); }
+}
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1400,
+        height: 600,
+        autoHideMenuBar: true,
+        resizable: false
+    })
+
+    mainWindow.loadFile('index.html')
+    mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
